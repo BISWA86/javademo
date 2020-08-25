8638b379df7b5bb6e9b32107c3486e10caa63cf7
BISWA86 <abiswajit10@gmail.com>
2020-08-25 15:09:23 -0400

    Indian team commit

--- src/main/java/com/example/demo/LimitController.java
+++ src/main/java/com/example/demo/LimitController.java
@@ -1,15 +1,18 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LimitController {
 	
+	@Autowired
+	private Configuration configuration;
 	
 	@GetMapping("/limits")
 	public LimitConfiguration retrieveLimitConfiguration(){
-		return new LimitConfiguration(1000,1);
+		return new LimitConfiguration(configuration.getMaximum(), configuration.getMinimum());
 		
 	}
 
